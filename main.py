b6df12961c8f8aebb80d5d075ba24e83507cf362
mrigaanksodhi <mrigaank.sodhi@education.nsw.gov.au>
2026-08-07 12:23:52 +1000

    Addition of skeleton
    In this lesson I added the buttons, heat detectors timers and everything. I started on the start of the main program, making the time and heat line/thershold which if passed would set of an alarm for 3 minutes after which a buzzer would ring. I still need to figure out how to make it so that if the heat readings go below the threshold it should stop.

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from machine import Pin, ADC
+from machine import Pin, ADC, PWM
 import time
 
+heat_line = 100
 
+
+heat_detected = 0
+heat_start_time = 0
+timer_active = 0  #if not active 0 if active then 1
+total_timer = 0  #total time left on timer even after stacking
+buzzer = 0
+auto_alarm_limit = 300
 
 #main program
 heat_sensor = ADC(Pin(26)) 
+timer_button = ADC(Pin(14))
+end_button = ADC(Pin(16))
+
 buzzer = Pin(20, Pin.out)
 led = Pin(16, Pin.out)
 
+while True:
+    current_time = time.time()
+    sensor_reading = heat_sensor.read()
+    timer_pressed = timer_button.value() #checks how many times it is pressed
 
+    if sensor_reading > heat_line:   #if the reading is more than 100 degrees it starts a timer for 3 minutes
+      heat_detected = 1
+      heat_start_time = current_time
+    if current_time - heat_start_time >= auto_alarm_limit:   # Once it has been more than 3 minutes the buzzer turns on
+         buzzer.value(1)
+    else: 
+       buzzer(0)
+    if timer_pressed:
+      if timer_active == 0:
 
+      
 
 
 
