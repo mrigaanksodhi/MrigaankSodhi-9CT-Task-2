f2e025331316e18ea2145e522c7bfb6bf2ffc3da
mrigaanksodhi <mrigaank.sodhi@education.nsw.gov.au>
2026-08-15 16:31:55 +1000

    Worked on the pin inputs, outputs and functions
    During this day I worked on the buttons sensors and stuff and what they were connected to. My mum helped me with some of the code helping me visualise what was happening and aiding in areas where I felt stuck. I  completely redid the led brightness as it was apparently c++ and not micropython , and rather added a simple algorithm that increases the led brightness with how much time is left.

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ heat_detected = 0
 heat_start_time = 0
 timer_active = 0  #if not active 0 if active then 1
 total_timer = 0  #total time left on timer even after stacking
+timer_start = 0
 buzzer = 0
 auto_alarm_limit = 300
 
 #main program
 heat_sensor = ADC(Pin(26)) 
-timer_button = ADC(Pin(14))
-end_button = ADC(Pin(16))
+timer_button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+end_button = Pin(15, Pin.IN, Pin.PULL_DOWN)
 
-buzzer = Pin(20, Pin.out)
-led = Pin(16, Pin.out)
+buzzer = Pin(20, Pin.OUT)
+led = PWM(Pin(16))
 
 while True:
     current_time = time.time()
@@ -25,60 +26,38 @@ while True:
     timer_pressed = timer_button.value() #checks how many times it is pressed
 
     if sensor_reading > heat_line:   #if the reading is more than 100 degrees it starts a timer for 3 minutes
-      heat_detected = 1
-      heat_start_time = current_time
-    if current_time - heat_start_time >= auto_alarm_limit:   # Once it has been more than 3 minutes the buzzer turns on
+      if heat_detected == 0:
+       heat_detected = 1
+       heat_start_time = current_time
+      if current_time - heat_start_time >= auto_alarm_limit:   # Once it has been more than 3 minutes the buzzer turns on
          buzzer.value(1)
+      else: 
+       heat_detected = 0
+       heat_start_time = 0
     else: 
-       buzzer(0)
+       buzzer.value(0)
+
     if timer_pressed:
-      if timer_active == 0:
 
-      
+        total_timer = total_timer + 300
 
+        if timer_active == 0:
+            timer_active = 1
+            timer_start = current_time
 
+        time.sleep(0.3)
 
-
-            #def start_timer(seconds):  Method 1
-                #while seconds >= 0:
-                    #seconds = seconds - 1
-                    #time.sleep(1)
-while time <= 300:   # Method 2
-    time = time + 1
-    time.sleep(1)
-    if time == 300:
-     print ('hello') #testing if it works
-
-# Automatic alarm function
-
-
-
-
-
-
-
-#timer set to 5 minutes function
-    #timer = 5 minutes (find out how to add)
-
-
-
-
-
-
-#stacking the 5 minutes function
+# All that is left is end button and working on timer function
 
 #led brightness
-    #LED value.0 slowly goes up
-    
-    int brightness = 0    #Sets initial brightness to 0 and goes upto 255
-    int fadeAmount = 1    #Goes up by 1 each time
-    void setup() {
-        pinMode(ledPin, OUTPUT);
-    }
-    void loop() {
-        brightness = brightness + fadeAmount; #changes the brightness
-    }
-    if brightness == 255:   #When brightness maxed it stops fade amount:
-        fadeAmount = 0
-    if timer == 0:          #When timer stops brightness auto maxes
-        brightness = 255
+    if timer_active == 1:
+        if total_timer > 240:
+            led.duty_u16(10000)
+        elif total_timer > 180:
+           led.duty_u16(20000)
+        elif total_timer > 120:
+            led.duty_u16(30000)
+        elif total_timer > 60:
+            led.duty_u16(45000)
+        else:
+           led.duty_u16(65000)
