d79773a284b44af36be9473e3cf803689268af9d
mrigaanksodhi <mrigaank.sodhi@education.nsw.gov.au>
2026-08-19 09:08:16 +1000

    Finished Code Entirely
    I had done this last night but forgot to save and commit. In this edit I finished the code, adding the end button function and editing the timer function. I also modified the led brightness function as I realised it should not be total_timer and it should be time_left. I added comments to explain what most lines did.

--- main.py
+++ main.py
@@ -31,8 +31,7 @@ while True:
             heat_start_time = current_time
         if current_time - heat_start_time >= auto_alarm_limit:   # Once it has been more than 3 minutes the buzzer turns on
             buzzer.value(1)
-        else: 
-            buzzer.value(0)
+        
     else: 
        heat_detected = 0
        heat_start_time = 0
@@ -40,27 +39,44 @@ while True:
 
     if timer_pressed:
 
-        total_timer = total_timer + 300
+        total_timer = total_timer + 300    #Adds 5 mins to the timer
 
-        if timer_active == 0:
-            timer_active = 1
-            timer_start = current_time
+        if timer_active == 0:  #If the timer is off currently
+            timer_active = 1   #Turn the timer on
+            timer_start = current_time  #Saves when the time started
 
-        time.sleep(0.3)
+        time.sleep(0.3)   #small delay
 
+    if timer_active == 1:
+        tp = current_time - timer_start   #Time passed
+        time_left = total_timer - tp
+        if time_left <= 0:
+            buzzer.value(1)
+            timer_active = 0   # timer turns off as its finished
+            total_timer = 0   # resets total timer
 
-# All that is left is end button and working on timer function
 
 #led brightness
     if timer_active == 1:
-        if total_timer > 240:
+        if time_left > 240:
             led.duty_u16(10000)
-        elif total_timer > 180:
+        elif time_left > 180:
            led.duty_u16(20000)
-        elif total_timer > 120:
+        elif time_left > 120:
             led.duty_u16(30000)
-        elif total_timer > 60:
+        elif time_left > 60:
             led.duty_u16(45000)
         else:
-           led.duty_u16(65000)
-        
+           led.duty_u16(65535)
+
+# End BUtton
+    if end_button.value() == 1:
+        buzzer.value(0)
+        led.duty_u16(0)
+        timer_active = 0
+        total_timer = 0
+        heat_detected = 0
+        heat_start_time = 0
+        time.sleep(0.5)
+
+    time.sleep(0.01)   #so pico doesnt overheat quickly
